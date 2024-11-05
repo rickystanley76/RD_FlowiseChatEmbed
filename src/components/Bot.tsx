9300fdff850f31af2b0c56fd76f8ca54870c4ddb
Ricky D'Cruze <rickystanley76@gmail.com>
2024-11-05 15:13:14 +0100

    Removing Brand2- RD

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -1608,12 +1608,6 @@ export const Bot = (botProps: BotProps & { class?: string }) => {
               />
             )}
           </div>
-          <Badge
-            footer={props.footer}
-            badgeBackgroundColor={props.badgeBackgroundColor}
-            poweredByTextColor={props.poweredByTextColor}
-            botContainer={botContainer}
-          />
         </div>
       </div>
       {sourcePopupOpen() && <Popup isOpen={sourcePopupOpen()} value={sourcePopupSrc()} onClose={() => setSourcePopupOpen(false)} />}
